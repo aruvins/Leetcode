f693c13f3929855a19e22d457bd1454a126878f5
aruvins <39351450+aruvins@users.noreply.github.com>
2024-09-14 11:28:44 -0400

    Leetcode 20: Valid Parentheses

---
+++ Blind75/Stacks/ValidParentheses_20.py
@@ -0,0 +1,62 @@
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+ 
+
+# Example 1:
+
+# Input: s = "()"
+
+# Output: true
+
+# Example 2:
+
+# Input: s = "()[]{}"
+
+# Output: true
+
+# Example 3:
+
+# Input: s = "(]"
+
+# Output: false
+
+# Example 4:
+
+# Input: s = "([])"
+
+# Output: true
+
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 104
+# s consists of parentheses only '()[]{}'.
+
+# Solution:
+# Since s only consists of '()[]{}' we can use a map to associated the opening to the closing
+# We then go through every element in s and check if c is not in the map, if it is append to the stack
+# The check if the stack is empty or the last element does not equal the element in the map(this one checks to see if the parenthesis is closed and in the proper position), return false if either of those conditions are met
+# other we pop the top element in the stack and continue in the loop
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        Map = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for c in s:
+            if c not in Map:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != Map[c]:
+                return False
+            stack.pop()
+
+        return not stack
+    
