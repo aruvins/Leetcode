1d6fbfa7785e0cd2022822912be4eaaebc4e7dbd
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-05-16 12:20:35 -0400

    Time: 31 ms (27.61%) | Memory: 37.6 MB (24.20%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -1,14 +1,14 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        smallestSeen = nums[0]
+        smallestSeen = float('inf')
         secondSmallest = float('inf')
 
-        for i in range(1,len(nums)):
-            if nums[i] < smallestSeen:
+        for i in range(len(nums)):
+            if nums[i] <= smallestSeen:
                 smallestSeen = nums[i]
-            if nums[i] < secondSmallest and nums[i] > smallestSeen:
+            elif nums[i] <= secondSmallest:
                 secondSmallest = nums[i]
-            if nums[i] > smallestSeen and nums[i] > secondSmallest:
+            else:
                 return True
 
         return False
