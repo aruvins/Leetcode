29e109bc57a350e13902955919c6faf79cdc9412
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-05-19 10:17:52 -0400

    Time: 1 ms (37.78%) | Memory: 17.9 MB (24.13%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,96 +1,15 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        pointerS = 0
+        l,r = 0,0
 
-        if s == '':
+        if s == "":
             return True
 
-        for c in t:
-            if s[pointerS] == c:
-                pointerS += 1
-            if pointerS == len(s):
+        while r < len(t):
+            if s[l] == t[r]:
+                l += 1
+            if l >= len(s):
                 return True
+            r += 1
 
         return False
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # sPointer = 0
-        # if s == '':
-        #     return True
-            
-        # for c in t:
-        #     if c == s[sPointer]:
-        #         sPointer +=1
-        #     if sPointer == len(s):
-        #         return True
-
-        # return False
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # if s == '':
-        #     return True
-
-        # stack = []
-        # for c in s:
-        #     stack.append(c)
-
-        # for i in range(len(t) -1 , -1, -1):
-        #     if stack and t[i] == stack[-1]:
-        #         stack.pop()
-                
-        # if not stack:
-        #     return True
-        # return False
