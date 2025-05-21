5cefada697edb820172efba9972e1707acb86812
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-05-21 11:38:13 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (65.75%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -2,11 +2,4 @@ from collections import Counter
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         freq = Counter(arr)
-        occurrencesSet = set()
-
-        for v in freq.values():
-            if v in occurrencesSet:
-                return False
-            occurrencesSet.add(v)
-
-        return True
+        return len(freq) == len(set(freq.values()))
