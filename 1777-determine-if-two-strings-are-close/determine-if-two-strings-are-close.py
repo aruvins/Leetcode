b04b3acbd07b4136be5af8e71a068d7c53edbb6d
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-05-22 11:50:35 -0400

    Time: 102 ms (52.82%) | Memory: 18.4 MB (20.73%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -4,4 +4,4 @@ class Solution:
         freq1 = Counter(word1)
         freq2 = Counter(word2)
 
-        return sorted(list(freq1.values())) == sorted(list(freq2.values())) and (set(word1) == set(word2))
+        return sorted(freq1.values()) == sorted(freq2.values()) and (set(word1) == set(word2))
