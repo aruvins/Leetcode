882cb53262c3442e0d8a1094001675f2f4fce068
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-07-07 11:28:36 -0400

    Time: 3 ms (65.72%) | Memory: 20.2 MB (6.85%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,8 +1,11 @@
-from collections import Counter
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        freq = Counter(nums)
+        seen = set()
 
-        for i,v in freq.items():
-            if v == 1:
-                return i
+        for n in nums:
+            if n in seen:
+                seen.remove(n)
+            else:
+                seen.add(n)
+
+        return seen.pop()
