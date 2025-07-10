320d7af4cee15d5234876288c8bdfef3b704f57d
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-07-10 10:26:51 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (43.14%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,26 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        #O(nlogn)
-        # for i,n in enumerate(nums2):
-        #     nums1[m + i] = n
+        for i,v in enumerate(nums2):
+            nums1[m+i] = v
 
-        # nums1.sort()
-
-
-        #Follow up: O(m + n)
-        last = m + n - 1
-
-        while m > 0 and n > 0:
-            if nums1[m - 1] > nums2[n - 1]:
-                nums1[last] = nums1[m - 1]
-                m -= 1
-            else:
-                nums1[last] = nums2[n - 1]
-                n -= 1
-            last -= 1
-
-        while n > 0:
-            nums1[last] = nums2[n - 1]
-            n, last = n - 1, last - 1
-            
+        nums1.sort()
