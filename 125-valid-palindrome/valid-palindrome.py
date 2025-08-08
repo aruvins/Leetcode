a6b48df1d55ec815257da3b7e7224de6d2ed9621
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-08-08 15:30:58 -0400

    Time: 11 ms (34.92%) | Memory: 23.3 MB (6.07%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,42 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l,r = 0, len(s) - 1
+        filtered = []
+        for c in s:
+            if c.isalnum():
+                filtered.append(c.lower())
 
-        while l < r:
-            if not s[l].isalnum():
-                l += 1
-                continue
-            if not s[r].isalnum():
-                r-=1 
-                continue 
-            if s[l].lower() != s[r].lower():
+        string = ''.join(filtered)
+
+        l, r = 0, len(string) - 1
+
+        while l <= r:
+            if string[l] != string[r]:
                 return False
             l += 1
             r -= 1
+
         return True
-
-
-
-
-
-
-
-
-
-
-
-    #     l, r = 0, len(s) - 1
-    #     while l < r:
-    #         while l < r and not self.alphaNum(s[l]):
-    #             l += 1
-    #         while r > l and not self.alphaNum(s[r]):
-    #             r -= 1
-    #         if s[l].lower() != s[r].lower():
-    #             return False
-    #         l, r = l+1, r- 1
-    #     return True
-    
-    # def alphaNum(self, c):
-    #         return (ord('A') <= ord(c) <= ord('Z') or 
-    #                 ord('a') <= ord(c) <= ord('z') or
-    #                 ord('0') <= ord(c) <= ord('9'))
