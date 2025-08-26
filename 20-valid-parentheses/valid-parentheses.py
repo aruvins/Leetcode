dc1449a18a1b19fce30a101863776a05b1eb1a99
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-08-26 11:42:01 -0400

    Time: 1 ms (33.62%) | Memory: 17.8 MB (80.92%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,15 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        hashMap = {'}':'{', ')': '(', ']':'['}
+        endings = {'}':'{', ')': '(', ']':'['}
         stack = []
 
         for c in s:
-            if c  in hashMap:
-                if stack and stack[-1] == hashMap[c]:
+            if c in endings:
+                if stack and stack[-1]== endings[c]:
                     stack.pop()
                 else:
                     return False
             else:
                 stack.append(c)
-
         return not stack
