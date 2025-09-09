9e9bee6d7277fed927603ca3fb8a5baff1a89f11
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-09-09 11:24:39 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (30.18%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -17,8 +17,8 @@ class Solution:
         one, two = 1, 1
 
         for _ in range(n - 1):
-            temp = one
-            one = one + two
-            two = temp
+            # temp = one
+            one, two = one + two, one
+            # two = temp
 
         return one
