32a6824db0aa2da76ab6833116a6f066dee082b0
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-09-22 17:11:59 -0400

    Time: 905 ms (9.37%) | Memory: 33.6 MB (5.24%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -73,40 +73,42 @@ class Solution:
 
 
         # # Heap Sort
-        # n = len(nums)
+        n = len(nums)
 
-        # def heapify(n, i):
-        #     largest = i
-        #     left = 2 * i + 1
-        #     right = 2 * i + 2
+        def heapify(n, root):
+            largest = root
+            left = 2 * root + 1
+            right = 2 * root + 2
 
-        #     if left < n and nums[left] > nums[largest]:
-        #         largest = left
+            #find largest among node and its children
+            if left < n and nums[left] > nums[largest]:
+                largest = left
 
-        #     if right < n and nums[right] > nums[largest]:
-        #         largest = right
+            if right < n and nums[right] > nums[largest]:
+                largest = right
 
-        #     if largest != i:
-        #         nums[i], nums[largest] = nums[largest], nums[i]
-        #         heapify(n, largest)
+            #if largest is not root, swap and recurse
+            if largest != root:
+                nums[root], nums[largest] = nums[largest], nums[root]
+                heapify(n, largest)
 
-        # for i in range(n//2 - 1, -1, -1):
-        #     heapify(n, i)
+        for i in range(n//2 - 1, -1, -1): #mid point to first element
+            heapify(n, i)
 
-        # for i in range(n - 1, 0, -1):
-        #     nums[0], nums[i] = nums[i], nums[0]
-        #     heapify(i,0)
+        for i in range(n - 1, 0, -1): 
+            nums[0], nums[i] = nums[i], nums[0]
+            heapify(i,0)
 
-        # return nums
+        return nums
 
 
 
 
         # Heap Sort (Built- in)
-        heapq.heapify(nums)
-        res = []
+        # heapq.heapify(nums)
+        # res = []
 
-        while nums:
-            res.append(heapq.heappop(nums))
+        # while nums:
+        #     res.append(heapq.heappop(nums))
 
-        return res
+        # return res
