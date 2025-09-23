04b32be5557e91da03ab0f483af5b5ce080d7075
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-09-23 11:29:37 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (78.38%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,24 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # memo = [-1] * (n + 1)
-        # def dfs(n):
-        #     if (n<0):
-        #         return 0
-        #     elif n == 0:
-        #         return 1
-        #     elif memo[n] != -1:
-        #         return memo[n]
-        #     else:
-        #         memo[n] = dfs(n-1) + dfs(n-2)
-        #         return memo[n]
-
-        # return dfs(n)
-
         one, two = 1, 1
 
-        for _ in range(n - 1):
-            # temp = one
+        for i in range(n - 1):
             one, two = one + two, one
-            # two = temp
 
         return one
