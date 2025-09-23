9283335e0c921e9012b49e3333d425a6d75f9293
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-09-23 12:02:41 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.99%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -16,13 +16,12 @@ class Solution:
 
         memo = {}
 
+        @cache
         def dfs(i):
             if i == 0:
                 return nums[0]
             if i == 1:
                 return max(nums[0], nums[1])
-            if i in memo:
-                return memo[i]
 
             memo[i] = max(dfs(i-1), nums[i] + dfs(i-2))
             return memo[i]
