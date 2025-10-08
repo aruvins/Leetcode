4d581e68b4a1a23894460fa9804d4c9645e53af6
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-10-08 16:16:05 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (95.82%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -4,53 +4,6 @@ class Solution:
 
         for i in range(1, len(strs)):
             while strs[i].find(prefix) != 0:
-                prefix = prefix[0:len(prefix)-1]
+                prefix = prefix[0:len(prefix) - 1]
 
         return prefix
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # commonPrefix = ""
-
-        # for i in range(len(strs[0])):
-        #     for word in strs:
-        #         if i == len(word) or word[i]!= strs[0][i]:
-        #             return commonPrefix
-        #     commonPrefix += strs[0][i]
-
-        # return commonPrefix
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # res = ""
-
-        # for i in range(len(strs[0])):
-        #     for s in strs:
-        #         if i == len(s) or s[i] != strs[0][i]:
-        #             return res
-        #     res += strs[0][i]
-        # return res
