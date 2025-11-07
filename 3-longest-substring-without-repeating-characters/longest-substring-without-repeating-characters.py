537dee3e16794ce22aa66257da5fdbbc680da9e3
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-11-07 15:04:39 -0500

    Time: 19 ms (54.14%) | Memory: 17.8 MB (52.53%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -8,7 +8,6 @@ class Solution:
             while s[r] in charSet:
                 charSet.remove(s[l])
                 l += 1
-
             charSet.add(s[r])
             res = max(res, r - l + 1)
 
