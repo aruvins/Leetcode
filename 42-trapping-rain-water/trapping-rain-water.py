7eb1a0085be9197ca761dfea401e83cbd3bb811b
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-11-24 10:08:16 -0500

    Time: 10 ms (73.36%) | Memory: 19.1 MB (93.01%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -3,7 +3,7 @@ class Solution:
         if not height:
             return 0
 
-        l,r = 0, len(height) - 1
+        l, r = 0, len(height) - 1
         leftMax, rightMax = height[l], height[r]
         res = 0
 
