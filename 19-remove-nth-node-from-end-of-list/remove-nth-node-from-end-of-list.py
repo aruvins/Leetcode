8940a461ce196d9658d67b4ec5a37d2471b7db9a
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-12-23 09:02:43 -0400

    Time: 0 ms (100.00%) | Memory: 17.3 MB (96.64%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -8,9 +8,11 @@ class Solution:
         dummy = ListNode(0, head)
         fast = slow = dummy
 
+        # Give fast pointer headstart by n nodes
         for _ in range(n + 1):
             fast = fast.next
 
+        # Keep going until end of list, slow pointer should be pointing at n because fast had the headstart
         while fast:
             fast = fast.next
             slow = slow.next
