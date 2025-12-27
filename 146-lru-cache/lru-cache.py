c055a68887ea9dfc9406923772425005da51183f
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2025-12-27 10:43:19 -0400

    Time: 112 ms (82.90%) | Memory: 77.8 MB (85.95%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,6 +1,6 @@
 class Node:
     def __init__(self, key, val):
-        self.key, self.val = key,val
+        self.key, self.val = key, val
         self.prev = self.next = None
 
 class LRUCache:
@@ -11,7 +11,7 @@ class LRUCache:
 
         self.left, self.right = Node(0,0), Node(0,0)
         self.left.next, self.right.prev = self.right, self.left
-
+        
     def remove(self, node):
         prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
@@ -23,11 +23,13 @@ class LRUCache:
 
     def get(self, key: int) -> int:
         if key in self.cache:
+            #recently used node so we need to update node's position in our cache to the front
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
         return -1
         
+
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
@@ -38,6 +40,8 @@ class LRUCache:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
+
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
