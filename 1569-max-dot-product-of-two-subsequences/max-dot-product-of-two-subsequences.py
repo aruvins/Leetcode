57b10b6677cba027f071c3581ae9ed816bc6ca2b
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2026-01-08 13:04:59 -0400

    Time: 123 ms (95.48%) | Memory: 22.2 MB (30.15%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.py
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.py
@@ -6,6 +6,12 @@ class Solution:
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 curProduct = nums1[i - 1] * nums2[j - 1]
-                dp[i][j] = max(dp[i][j], curProduct, dp[i-1][j], dp[i][j - 1], curProduct + dp[i - 1][j - 1])
+
+                dp[i][j] = max(
+                    curProduct,
+                    curProduct + dp[i - 1][j - 1],
+                    dp[i - 1][j],
+                    dp[i][j - 1] 
+                )
 
         return dp[m][n]
