493f8e1074ee0d32b159edcc4b825c8b22402994
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2026-01-15 11:25:54 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (13.85%) - LeetSync

---
+++ 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maximizeSquareHoleArea(self, n: int, m: int, hBars: List[int], vBars: List[int]) -> int:
+        def maxSpan(bars):
+            bars.sort()
+            res = 1
+            streak = 1
+
+            for i in range(1, len(bars)):
+                if bars[i] - bars[i - 1] == 1:
+                    streak += 1
+                else:
+                    streak = 1
+                res = max(res, streak)
+
+            return res + 1
+
+        return min(maxSpan(hBars), maxSpan(vBars)) ** 2
