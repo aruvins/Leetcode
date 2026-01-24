817483d0fd1ddc80c8b119463c3559d6d72cc3f6
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2026-01-24 11:41:48 -0500

    Time: 0 ms (100.00%) | Memory: 21.2 MB (8.14%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -9,8 +9,8 @@ class Solution:
         def helper(node, left, right):
             if not node:
                 return True
-
-            if not(node.val > left and node.val < right):
+            
+            if not (node.val > left and node.val < right):
                 return False
 
             return helper(node.left, left, node.val) and helper(node.right, node.val, right)
