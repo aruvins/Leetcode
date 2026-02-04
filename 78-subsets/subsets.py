444c11f750cc31b3bf70657d6c3561b7b4ba92df
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2026-02-04 12:48:16 -0500

    Time: 0 ms (100.00%) | Memory: 19.6 MB (15.99%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,14 +1,23 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
+        subset = []
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
 
-        def backtrack(start, path):
-            res.append(path[:])
 
-            for i in range(start, len(nums)):
-                path.append(nums[i])
-                backtrack(i + 1, path)
-                path.pop()
+            # decision to include nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
 
-        backtrack(0, [])
+            # decision NOT to include nums[i]
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
         return res
+            
+            
+            
