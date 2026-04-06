39076a7c2e66440bd7b75467dc7e1e56fb87c471
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2026-04-06 11:43:22 -0400

    Time: 3 ms (63.42%) | Memory: 19.3 MB (61.37%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,13 +1,8 @@
-from functools import cache
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        
-        @cache
-        def dp(n):
-            if n < 2:
-                return cost[n]
+        cost.append(0)
 
-            return cost[n] + min(dp(n - 1), dp(n - 2))
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] = cost[i] + min(cost[i + 1], cost[i + 2])
 
-        length = len(cost)
-        return min(dp(length - 1), dp(length - 2))
+        return min(cost[0], cost[1])
