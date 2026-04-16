c39a407374ee20e53cd80f91a13f1b25b91b0e44
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2026-04-16 08:53:47 -0400

    Time: 283 ms (53.30%) | Memory: 19.1 MB (98.45%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,29 +1,28 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) <= 1:
-            return s
-
-        maxLen = 1
-        maxStr = s[0]
-        s = '#' + '#'.join(s) + '#'
-        dp = [0 for _ in range(len(s))]
-        center = 0
-        right = 0
+        res = ""
+        resLen = 0
 
         for i in range(len(s)):
-            if i < right:
-                dp[i] = min(right - i, dp[2*center - i])
+            # Odd length
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+
+            # Even length
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+
+        return res
             
-            while i - dp[i] - 1 >= 0 and i + dp[i] + 1 < len(s) and s[i - dp[i] - 1] == s[i + dp[i] + 1]:
-                dp[i] += 1
-
-            if i + dp[i] > right:
-                center = i
-                right = i + dp[i]
-
-            if dp[i] > maxLen:
-                maxLen = dp[i]
-                maxStr = s[i-dp[i]: i + dp[i]+1].replace("#",'')
-
-        return maxStr
-
