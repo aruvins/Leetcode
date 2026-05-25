daedc4a0458e7cb2c108d7678d0bd8ebeab803ed
Aidan Ruvins <39351450+aruvins@users.noreply.github.com>
2026-05-25 07:29:38 -0400

    Time: 7 ms (5.38%) | Memory: 21.6 MB (7.37%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -3,12 +3,12 @@ class Solution:
         res = []
 
         for i in range(len(intervals)):
-            if newInterval[1] < intervals[i][0]: # Append to the beginning
+            if newInterval[1] < intervals[i][0]: # Append to the beginning (new Interval's end value is less than the current intervals start value)
                 res.append(newInterval)
                 return res + intervals[i:]
-            elif newInterval[0] > intervals[i][1]: # Append to the end
+            elif newInterval[0] > intervals[i][1]: # Append to the end (new Interval's start value is greater than the current intervals end value)
                 res.append(intervals[i])
-            else: # Overlapping interval
+            else: # Overlapping interval (Grab the minimum start value and max end value)
                 newInterval = [
                     min(newInterval[0], intervals[i][0]),
                     max(newInterval[1], intervals[i][1])
